441cde6d2b88049d22952272b3c5e71a1a704e5b
velor2012 <38395332+velor2012@users.noreply.github.com>
2024-01-24 10:25:07 +0800

    适配4.2.0，调整页脚样式

--- themes/easy/components/Footer.js
+++ themes/easy/components/Footer.js
@@ -1,6 +1,6 @@
 import { siteConfig } from '@/lib/config'
 
-const Footer = ({ title }) => {
+const Footer = () => {
   const d = new Date()
   const currentYear = d.getFullYear()
   const since = siteConfig('SINCE')
@@ -16,13 +16,7 @@ const Footer = ({ title }) => {
                 <i className='fas fa-copyright' /> {`${copyrightDate}`} <span className='mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={siteConfig('LINK')} className='underline font-bold '>{siteConfig('AUTHOR')}</a>.<br />
 
                 {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br /></>}
-
-                <span className='hidden busuanzi_container_site_pv'>
-                    <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span> </span>
-                <span className='pl-2 hidden busuanzi_container_site_uv'>
-                    <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-                <br />
-                <h1>{title}</h1>
+                
                 <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {siteConfig('VERSION')}</a>.</span>
             </span>
         </footer>
