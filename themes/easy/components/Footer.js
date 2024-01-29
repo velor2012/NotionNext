97bffa3f5570680b0b215848b614bbca885d3955
velor2012 <38395332+velor2012@users.noreply.github.com>
2024-01-29 18:33:16 +0800

    添加萌备

--- themes/easy/components/Footer.js
+++ themes/easy/components/Footer.js
@@ -17,6 +17,8 @@ const Footer = () => {
 
                 {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br /></>}
                 
+                {siteConfig('MOE_BEI') && <><i className='fas fa-star' /> <a href={`https://icp.gov.moe/?keyword=${siteConfig('MOE_BEI')}`} target="_blank">{`萌ICP备${siteConfig('MOE_BEI')}号`}</a><br /></>}
+                
                 <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {siteConfig('VERSION')}</a>.</span>
             </span>
         </footer>
