b328628e74d5fb4be684d223a1a7f3877628fa68
velor2012 <38395332+velor2012@users.noreply.github.com>
2024-03-28 12:06:06 +0800

    调整归档页面，由新到旧排序

--- themes/easy/components/BlogPostArchiveList.js
+++ themes/easy/components/BlogPostArchiveList.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import BlogPostArchive from './BlogPostArchive'
 import { AnimatePresence, motion } from 'framer-motion'
 
@@ -22,11 +22,25 @@ const variants = {
     }
   }
 
+  const sortByStartDate = (arrs) =>{
+    const k1 = 'date'
+    const k2 = 'start_date'
+    return arrs.sort((a, b) => {
+        if (a[k1][k2] > b[k1][k2]) {
+            return -1;
+        }
+        if (a[k1][k2] < b[k1][k2]) {
+            return 1;
+        }
+        return 0;
+    });
+  }
+
 const BlogPostArchiveList = props => {
   const { archivePosts } = props
   // 按五年的区间分组，页面上每页展示五年的数据
   const convertToArrs = (posts) => {
-    debugger
+    
     const sortedKeys = Object.keys(posts).sort();
 
     const result = sortedKeys.reduce((acc, key) => {
@@ -38,15 +52,18 @@ const BlogPostArchiveList = props => {
       if (!acc[groupKey]) {
         acc[groupKey] = {};
       }
-    
-      acc[groupKey][key] = posts[key];
+      acc[groupKey][key] = sortByStartDate(posts[key]);
     
       return acc;
     }, []);
     return result;
   }
   
-  const datas = convertToArrs(archivePosts)
+  const datas = useMemo(() => convertToArrs(archivePosts), [archivePosts])
+  const keysSortDesc = useMemo(()=>{
+    const keys = Object.keys(datas)
+    return keys.reverse()
+  },[datas])
   const [curKey, setCurKey] = useState(Object.keys(datas)[0])
   const [curPosts, setCurPosts] = useState([])
   const [curVariants, setCurVariants] = useState(variants)
@@ -62,7 +79,7 @@ const BlogPostArchiveList = props => {
   }
 
   useEffect(() => {
-    const k = Object.keys(datas)[0]
+    const k = keysSortDesc[0]
     setCurKey(k)
     setCurPosts(datas[k])
     console.log("curKey: ", curKey)
@@ -74,7 +91,7 @@ const BlogPostArchiveList = props => {
             <div className='dark:text-gray-200 mb-5'><i className='fas fa-box-archive mr-4' />归档年份</div>
             <div id='archive-interval-list' className='duration-200 flex flex-wrap'>
                 {
-                    Object.keys(datas).map(curInterval => 
+                    keysSortDesc.map(curInterval => 
                         <motion.div 
                         key={curInterval}
                         className={`z-0 mr-2 p-2 ${curKey != curInterval ? 'hover:scale-110 duration-100 transform': ''} cursor-pointer 
@@ -101,7 +118,7 @@ const BlogPostArchiveList = props => {
         </div>
         <div className=" min-h-screen rounded-md mb-10 pb-20 bg-white md:p-12 p-3 md:pt-0 pt-0 dark:bg-hexo-black-gray shadow-md min-h-full">
         <AnimatePresence mode='wait'>
-            {datas[curKey] && Object.keys(curPosts).map(archiveTitle => (
+            {datas[curKey] && Object.keys(curPosts).reverse().map(archiveTitle => (
                 <motion.div
                 initial="hidden" animate="show" variants={variants} exit="exit"
                 key={archiveTitle}
